9e2011c3d7f5e45cae9f9bdbae894a2d1d48c2ec
Priyanka Mane <64961583+priyankamane07@users.noreply.github.com>
2023-02-14 18:50:57 +0530

    Add files via upload
    test commit to new branch

---
+++ rem.py
@@ -0,0 +1,15 @@
+
+def find_remainder(arr,n):
+
+    sum_1=reduce(lambda x,y: x*y,arr)
+
+    remainder=sum_1%n
+
+    print(remainder)
+
+    arr=[100,10,5,25,35,14]
+
+    n=11
+
+    find_remainder(arr,n)
+
